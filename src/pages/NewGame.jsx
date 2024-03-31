ae359850d1daa9175e79c28fbb0d44fa82be4d7d
Max Rubel <maxrubel1@gmail.com>
2024-03-30 20:11:32 -0500

    new game listens to server

--- src/pages/NewGame.jsx
+++ src/pages/NewGame.jsx
@@ -1,38 +1,71 @@
 import { useSearchParams } from "react-router-dom";
 import "../styles/newGame.scss";
 import GameContext from "../GameContext";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { updateGame } from "../../api/game";
+import firebase from "firebase/compat/app";
+
 
 export default function NewGame() {
+  const db = firebase.database();
+
   const [searchParams] = useSearchParams();
   const gameId = searchParams.get("gameId");
-  const { player1, player2, updatePlayer1, updatePlayer2 } =
-    useContext(GameContext);
+  const [gameData, setGameData] = useState({
+    player1: false,
+    player2: false
+  })
+  const { isPlayer, selectPlayer } = useContext(GameContext);
+
+  useEffect(() => { //firebase listener function
+    const listenerRef = db.ref('games').orderByChild('gameId').equalTo(gameId);
+
+    const onGameUpdated = (snapshot) => {
+      const data = snapshot.val()
+      if (data) {
+        const dataFormatted = (Object.values(snapshot.val()))[0]
+        setGameData((preVal) => dataFormatted)
+      }
+    };
+
+    const listener = listenerRef.on('value', onGameUpdated);
+
+    return () => {
+      listenerRef.off('value', listener);
+    };
+  }, [gameId]);
+
 
   const handlePlayer1 = () => {
-    updatePlayer1(true);
-    const payload = { gameId, player1: true };
-    updateGame(payload);
+    if (!isPlayer) {
+      selectPlayer(1)
+      const payload = { gameId, player1: true };
+      updateGame(payload);
+    } else {
+      window.alert('you are already playing')
+    }
   };
 
   const handlePlayer2 = () => {
-    updatePlayer2(true);
-    const payload = { gameId, player2: true };
-    updateGame(payload);
+    if (!isPlayer) {
+      selectPlayer(2)
+      const payload = { gameId, player2: true };
+      updateGame(payload);
+    } else {
+      window.alert('you are already playing')
+    }
   };
-
-  if (player1 && player2) {
-    console.log("Start Game");
-  }
 
   return (
     <>
       <div className="new-game-container">
-        {player1 ? (
-          <button>Player 1 has joined ⚔️</button>
+        {gameData.player1 ? (
+          <button
+            className="width300"
+            disabled>Player 1 has joined ⚔️</button>
         ) : (
           <button
+            className="width300"
             onClick={() => {
               handlePlayer1();
             }}
@@ -40,10 +73,13 @@ export default function NewGame() {
             Join Player 1
           </button>
         )}
-        {player2 ? (
-          <button>Player 2 has joined 🧙‍♂️</button>
+        {gameData.player2 ? (
+          <button
+            className="width300"
+            disabled>Player 2 has joined 🧙‍♂️</button>
         ) : (
           <button
+            className="width300"
             onClick={() => {
               handlePlayer2();
             }}
